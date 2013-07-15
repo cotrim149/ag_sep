fb551572b076bc6b7033f181a5680a457fae932b
Felipe Cesar Silveira de Assis <fel.cesar@gmail.com>
2013-07-15 03:07:19 -0300

    PROXIMO PASSO>> FAZER SELEÇÂO DOS PAIS de pois cruzamento

--- ag/ag.cpp
+++ ag/ag.cpp
@@ -13,7 +13,40 @@ int valor_aleatorio(int denominador){
 	
 	return valor%denominador;
 }
+void imprime_vetor(int * vetor, int tamanho){
+	cout<< "Imprimindo vetor: "<< endl;
+	for(int i=0; i<tamanho;i++){
+		cout<<"vetor["<<i<<"]= "<< vetor[i]<<endl;
+	}
 
+}
+void imprime_vetorf(float * vetor, int tamanho){
+	cout<< "Imprimindo vetor: "<< endl;
+	for(int i=0; i<tamanho;i++){
+		cout<<"vetor["<<i<<"]= "<< vetor[i]<<endl;
+	}
+
+}
+
+int soma_vetor(int * vetor,int tamanho){
+	int soma =0;
+	for(int i=0; i<tamanho;i++){
+		soma +=vetor[i];
+		//cout<<soma<<endl;
+	}
+
+	return soma;
+}
+
+float soma_vetorf(float * vetor,int tamanho){
+	float soma =0;
+	for(int i=0; i<tamanho;i++){
+		soma +=vetor[i];
+		//cout<<soma<<endl;
+	}
+
+	return soma;
+}
 
 void mutacao(int* cromossomo,int tamanho_cromossomo){
 	int opcao = valor_aleatorio(2);
@@ -77,78 +110,136 @@ void crossover(int* pai_1,int tamanho_pai_1, int* pai_2, int tamanho_pai_2){
 	
 }
 
+//inicializa com zeros a população e em seguida infecta uma quantiodade determinada de individuos, pode ser por completo ou aleatorio
+void inicia_populacao(int  ** matriz_pop , int numero_cromossomos, int tamanho_pai ,int qtd_infect){
+	
+	//inicializa a população com 0s
+	numero_cromossomos -=1;
+	cout<<endl;
+	for(int i=0; i <=numero_cromossomos;i++){
+		cout<< "INICIANDO população: "<< "<"<< i <<"> = "<<endl;
+		for(int j=0;j<tamanho_pai;j++){
+			matriz_pop[i][j]=0;
+			cout<< matriz_pop[i][j] ;
+		}
+		cout<< endl;
+	}
+	
+	
+	//inicializa com a quantidade de infectados totalmente
+	for(int i=0; i<= (qtd_infect-1); i++){
+		int rand = valor_aleatorio(numero_cromossomos+1);
+				for(int j=0;j<tamanho_pai;j++){
+				matriz_pop[rand][j] = valor_aleatorio(2);
+				//matriz_pop[rand][j] = 1; //para uma infecção total do individuo
+				}
+				cout<< "INFECÇÃo>> "<<rand<<endl;
+
+			
+	}
+}
+
+//apenas imprime a população
+void imprime_populacao(int ** matriz_pop,int numero_cromossomos, int tamanho_pai){
+	numero_cromossomos -=1;
+	for(int i=0; i <=numero_cromossomos;i++){
+		cout<< "INICIANDO população: "<< "<"<< i <<"> = "<<endl;
+		for(int j=0;j<tamanho_pai;j++){
+			cout<< matriz_pop[i][j] ;
+		}
+		cout<< endl;
+	}
+
+
+}
+
+int * calcula_fitness(int ** matriz_pop,int numero_cromossomos, int tamanho_pai){
+	int  * notas;
+	notas = new int[numero_cromossomos];
+	//inicializando vetor de notas
+	for(int i=0; i<numero_cromossomos;i++){
+		notas[i]=0;
+	}
+	
+	//somando a quantidades de 1s em cada cromossomo (FUNÇÂO FITNESS)
+	for(int i=0; i<numero_cromossomos;i++){
+		for(int j=0;j<tamanho_pai;j++){
+			notas[i] += matriz_pop[i][j];// função fitness, pode seralterada aqui
+			
+		}
+		if(notas[i]==0) notas[i]=1;// para que quem nao tenha sintomas tenha chance de ser selecionado
+	}
+	
+	imprime_vetor(notas,numero_cromossomos);
+	
+	
+	cout <<endl<< "PASSOU"<<endl;
+	
+	return notas;
+}
+float *calcula_pesos(int * fitness , int numero_cromossomos){
+
+	float * pesos = new float[numero_cromossomos];
+	float soma_fitness = soma_vetor(fitness,numero_cromossomos);
+	cout<< soma_fitness;
+	for(int i=0; i<numero_cromossomos;i++){
+		pesos[i]= ( 1 * fitness[i] )/soma_fitness;
+		cout<<pesos[i]<<endl;
+	}
+	
+	
+	return pesos;
+}
+
+void 
+
+
 int main(){
 	system("cls");
-	int cromossomo[qtd_genes];
+	srand(time(NULL));		
 
-	srand(time(NULL));
+//VALORES DEFINIDOS PARA INICIALIZAÇÃO
+	int numero_cromossomos = 10;//numero de individuos
+	int tamanho_pai=10;//numero de genes
+	//int pop[numero_cromossomos][tamanho_pai]; inicializando a matriz de elementos
+	int **pop;
+	pop = new int *[numero_cromossomos];
+	for(int i = 0; i <10; i++)
+    pop[i] = new int[tamanho_pai];	
 
 
-	for(int i=0; i< qtd_genes; i++)
-		cromossomo[i] =valor_aleatorio(2);
-		
-	cout << "antes" << endl;
-	/*for(int i=0; i< qtd_genes; i++)
-		cout << "gene" << i+1 << ": " << cromossomo[i] << endl;
-	*/
-	int numero_cromossomos = 2;
-	int tamanho_pai=4;
-	
-	int pop[numero_cromossomos][tamanho_pai];
-	
-	int pai_1[tamanho_pai];
-	int pai_2[tamanho_pai];
+	//int pai_1[tamanho_pai];
+	//int pai_2[tamanho_pai];
 	
 	//inicialização de teste
-
-	
-	pop[0][0]=0;
-	pop[0][1]=1;
-	pop[0][2]=1;
-	pop[0][3]=1;
-	
-	pop[1][0]=0;
-	pop[1][1]=1;
-	pop[1][2]=0;
-	pop[1][3]=0;
-	
-	pai_1[0]=pop[0][0];
-	pai_1[1]=pop[0][1];
-	pai_1[2]=pop[0][2];
-	pai_1[3]=pop[0][3];
-	
-	pai_2[0]=pop[1][0];
-	pai_2[1]=pop[1][1];
-	pai_2[2]=pop[1][2];
-	pai_2[3]=pop[1][3];
-	
-	cout << "pai 1 ";
-	for(int i=0; i< tamanho_pai; i++)
-		cout << " " << pai_1[i];
-	
-	cout << "pai 2 ";
-	for(int i=0; i< tamanho_pai; i++)
-		cout << " " << pai_2[i];
-	
-	
 	//testando crossover
-	crossover(pai_1,tamanho_pai,pai_2,tamanho_pai);	
+	//crossover(pai_1,tamanho_pai,pai_2,tamanho_pai);	
 	//mutacao(pai_1,tamanho_pai);
 	//mutacao(pai_2,tamanho_pai);
 	
-	cout << "depois" << endl;
+	//cout << "depois" << endl;
 	/*for(int i=0; i< qtd_genes; i++)
 		cout << "gene" << i+1 << ": " << cromossomo[i] << endl;
 	*/
 	
-	cout << "pai 1 ";
-	for(int i=0; i< tamanho_pai; i++)
-		cout << " " << pai_1[i];
 	
-	cout << "pai 2 ";
-	for(int i=0; i< tamanho_pai; i++)
-		cout << " " << pai_2[i];
+	
+	inicia_populacao(pop,numero_cromossomos,tamanho_pai,3);
+	imprime_populacao(pop,numero_cromossomos,tamanho_pai);
+	
+		
+	//inicia o loop
+	int * fitness;
+	float * pesos;
 
+	fitness = calcula_fitness(pop,numero_cromossomos,tamanho_pai);
+	imprime_vetor(fitness, numero_cromossomos);
+	
+	pesos = calcula_pesos(fitness,numero_cromossomos);
+	
+	imprime_vetorf(pesos,numero_cromossomos);
+	
+	cout<< soma_vetorf(pesos,numero_cromossomos);
 return 0;
 }
 
